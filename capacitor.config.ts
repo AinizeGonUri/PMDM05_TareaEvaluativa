9f5848ea72f373c8fd8f06b0656b18c52b40f982
AinizeGonUri <ainigonzalez@birt.eus>
2025-02-14 11:53:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PMDM04_TareaEvaluativa01',
+  webDir: 'www'
+};
+
+export default config;
